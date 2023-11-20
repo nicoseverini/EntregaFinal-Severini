1a75d45c6a14f42b4a0614cff21f24150f2462ff
Nicolas Severini <nicoseve823@gmail.com>
2023-11-20 02:40:15 -0300

    Entrega2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
+import MainRoutes from './routes/MainRouter';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBarComponent from './components/NavBar/NavBarComponent';
-import ItemListContainer from './components/ItemList/ItemListContainer';
 function App() {
 
 
   return (
     <div>
-      <NavBarComponent />
-      <ItemListContainer greeting="Bienvenidos!!!" />
+      <MainRoutes />
     </div>
   )
 }
--- src/components/CartWidgetComponent/CartWidgetComponent.jsx
+++ src/components/CartWidgetComponent/CartWidgetComponent.jsx
@@ -1,6 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faCartShopping} from '@fortawesome/free-solid-svg-icons';
-import { icon } from '@fortawesome/fontawesome-svg-core';
 const CartWidgetComponent = () => {
   const iconStyle = {
     fontSize: "20px",
